f9ad5582066d1976b0d49c3109269cdd832186f3
Mallow <linlam0923@gmail.com>
2023-01-25 23:19:51 -0500

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,17 @@
 const parseCookies = (req, res, next) => {
+  if (req.headers.cookie === undefined) {
+    return {};
+  }
+
+  var requestCookie = req.headers.cookie;
+  requestCookie = requestCookie.split('; ');
+  var cookies = {};
+  requestCookie.forEach((cookie) => {
+    cookie = cookie.split('=');
+    cookies[cookie[0]] = cookie[1];
+  });
+  req.cookies = cookies;
+  next();
 };
 
 module.exports = parseCookies;
